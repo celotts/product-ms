3941866756e07afbba134e6045965b784a00b3f9
Carlos Lott <celotts@gmail.com>
2025-04-16 20:23:51 -0600

    init: configuración base del microservicio product-ms con Docker y build

---
+++ infra/docker/product-ms/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Esperando a que la base de datos esté lista..."
+/app/wait-for-it.sh -t 60 product-db:5432 --strict -- echo "Base de datos lista!"
+
+echo "Iniciando aplicación..."
+exec java -jar app.jar
